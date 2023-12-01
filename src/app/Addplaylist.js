001733ac46f8d2385ff26e79501da0fb675e6f0e
andrewcwin <andrewcwin@gmail.com>
2023-11-30 22:10:32 -0600

    login and attempt to search for playlists

--- src/app/Addplaylist.js
+++ src/app/Addplaylist.js
@@ -1,5 +1,29 @@
 const axios = require('axios');
 const qs = require('qs');
+const express = require('express');
+const querystring = require('querystring');
+require('dotenv').config();
+
+var client_id = process.env.SPOTIFY_CLIENT_ID;
+
+// var client_id = SPOTIFY_CLIENT_ID; // Your client id <- DOESNT WORK
+var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+
+var app = express();
+
+app.get('/login', function(req, res) {
+  var state = generateRandomString(16);
+  var scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
 
 // Step 1: Authenticate and get access token
 const data = {
@@ -55,4 +79,36 @@ axios(options)
   })
   .catch(error => {
     console.error(`Error getting access token: ${error}`);
+    console.log(error);
   });
+
+
+  app.get('/search', function(req, res) {
+    const trackName = req.query.trackName; // Get the track name from the query parameters
+  
+    // Search for tracks by name
+    axios({
+      method: 'GET',
+      url: 'https://api.spotify.com/v1/search',
+      headers: {
+        Authorization: `Bearer ${accessToken}`, // Use your access token here
+      },
+      params: {
+        q: trackName, // Use the track name from the query parameters
+        type: 'track',
+        limit: 5, // Limit the number of results
+      },
+    })
+    .then(response => {
+    const tracks = response.data.tracks.items;
+    res.json(tracks); // Send the tracks as a JSON response
+  })
+  .catch(error => {
+    // console.error(`Error searching for tracks: ${error}`);
+    res.status(500).send('Error searching for tracks'); // Send an error response
+  });
+});
+
+  
+
+  app.listen(3000);
